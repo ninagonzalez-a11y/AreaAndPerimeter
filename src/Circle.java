757394fa28f94fbce037a0d50412057c381a7e87
Nina <nina_gonzalez@dlsu.edu.ph>
2026-06-01 16:15:21 +0800

    added annotations to the circle class

--- src/Circle.java
+++ src/Circle.java
@@ -1,19 +1,24 @@
 public class Circle {
+    // [UNDERSTAND] Radius variable restricts access to other classes
     private double radius;
 
+    // [UNDERSTAND] Initialized the radius variables
     public Circle(){
         this.radius = 0.0;
     }
 
+    // [DECISION] Used a constructor to create the circle object and input the radius as data to be used later
     public Circle(double radius){
         setRadius(radius);
     }
 
+    // [UNDERSTAND] This is the getter method
     public double getRadius(){
         return radius;
     }
-
+    // [UNDERSTAND] This is the setter method
     public void setRadius(double radius){
+        // [DECISION] Used if-else statement to prevent negative radius inputs
         if(radius>=0){
             this.radius = radius;
         }
@@ -24,14 +29,20 @@ public class Circle {
         }
 
     public double calculateArea() {
+        // [UNDERSTAND] Declared the 'a' variable for area
         double a;
+        // [UNDERSTAND] Set the formula for the area of a circle
         a = Math.PI * radius * radius;
+        // [UNDERSTAND] Returned a double value
         return a;
     }
 
     public double calculatePerimeter(){
+        // [UNDERSTAND] Declared the 'p' variable
         double p;
+        // [UNDERSTAND] Set the formula for the perimeter of a circle
         p = 2.0*Math.PI*radius;
+        // [UNDERSTAND] Returned a double value
         return p;
     }
 
