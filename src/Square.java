48a067123ae0ccd00ab8824c7fbe51d0e9c8952b
Nina <nina_gonzalez@dlsu.edu.ph>
2026-06-01 12:53:03 +0800

    added getter and setter in square class

---
+++ src/Square.java
@@ -0,0 +1,27 @@
+public class Square {
+    private double side;
+    // getter methods
+    double getSide(){
+        return side;
+    }
+
+    // setter methods
+    void getSide(double side){
+        this.side = side;
+    }
+
+    public double calculateArea(){
+
+
+
+
+
+    }
+
+    public double calculatePerimeter(){
+        double p;
+        p = 4.0*side;
+        return p;
+    }
+
+}
