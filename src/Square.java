2a312e1d36ce1a2375d661fa3e16f3f7d3ea4cc9
Nina <nina_gonzalez@dlsu.edu.ph>
2026-06-01 16:59:19 +0800

    added annotations to the square class

--- src/Square.java
+++ src/Square.java
@@ -1,22 +1,24 @@
 public class Square {
+    // [UNDERSTAND] Declared a side variable as double restricting the access to other classes and methods
     private double side;
 
-// constructor #1
+// [UNDERSTAND] Initialized the side variable in a constructor
     public Square(){
         this.side = 0.0;
     }
-// setter method as constructor #2
+// [DECISION] Used a constructor to create square object and added the input side as data to be used later
     public Square(double side){
         setSide(side);
     }
 
-// getter method
+// [UNDERSTAND] Getter method for the side
     public double getSide(){
         return side;
     }
 
-// setter method
+// [UNDERSTAND] Setter method for the side
     public void setSide(double side){
+       // [UNDERSTAND] Used an if-else statement to prevent the inputted side from becoming negative
         if(side>=0){
             this.side = side;
         }
@@ -24,16 +26,22 @@ public class Square {
             System.out.println("Invalid! There should be no negative numbers!");
         }
     }
-
+//[UNDERSTAND] Method to calculate the area for square
     public double calculateArea(){
+        // [UNDERSTAND] Declared 'a' variable as area and double data type
         double a;
+        // [UNDERSTAND] Set the formula for area of square
         a = side*side;
+        // [UNDERSTAND] Returned a double value
         return a;
     }
-
+//[UNDERSTAND] Method to calculate the perimeter for square
     public double calculatePerimeter(){
+        // [UNDERSTAND] Declared 'p' as perimeter and double data type
         double p;
+        // [UNDERSTAND] Set the formula for perimeter of a square
         p = 4.0*side;
+        // [UNDERSTAND] Returned a double value
         return p;
     }
 
